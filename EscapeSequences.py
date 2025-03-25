23594bf27a9e82f15cf047e2569f1d4fbf7e7d2c
Aarthi3730 <aarthikodipalli@gmail.com>
2025-03-26 01:12:09 +0530

    Create EscapeSequences.py

---
+++ EscapeSequences.py
@@ -0,0 +1,21 @@
+'''
+message="Python Programming"
+print(message)
+'''
+'''
+#\"
+#\'
+#\\
+#\n
+'''
+message="Python \"Programming"
+print(message)
+message="Python \\Programming"
+print(message)
+message="Python \'Programming"
+print(message)
+message="Python \nProgramming"
+print(message)
+
+
+
