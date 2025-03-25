b0a8ecc5bf7773ecfc38ab80b142ad06729a4ff3
Aarthi3730 <aarthikodipalli@gmail.com>
2025-03-26 01:23:23 +0530

    Create UsefulStringMethod.py

---
+++ UsefulStringMethod.py
@@ -0,0 +1,9 @@
+course="Python Programming"
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip())
+print(course.find("Pro"))
+print(course.replace("P","_"))
+print("Programming" in course)
+print("Programming"not in course)
