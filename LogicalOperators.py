1158e8b5f627e9f9a259d98544b6adca02ffaa80
Aarthi3730 <aarthikodipalli@gmail.com>
2025-03-27 23:01:24 +0530

    Create LogicalOperators.py

---
+++ LogicalOperators.py
@@ -0,0 +1,16 @@
+# Logical AND
+x = 10
+y = 20
+if x > 5 and y > 15:
+    print("Both conditions are True")
+
+# Logical OR
+a = 5
+b = 2
+if a > 10 or b < 3:
+    print("At least one condition is True")
+
+# Logical NOT
+is_raining = False
+if not is_raining:
+    print("It's not raining, you can go outside!")
