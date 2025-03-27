7c4d43d86e3a31dde9aadbf22cc7fae8147ac94f
Aarthi3730 <aarthikodipalli@gmail.com>
2025-03-27 23:18:27 +0530

    Create ForLoops.py

---
+++ ForLoops.py
@@ -0,0 +1,10 @@
+for x in "python":
+    print(x)
+for x in ['1','2','3']:
+    print(x)
+for x in range(0,5):
+    print(x)
+for x in range(2,5):
+    print(x)
+for x in range(0,10,2):
+    print(x)
